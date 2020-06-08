ac1e685950a2f9badc15ec9ad54f6fb4ec023351
Justin Montgomery <montgomerycodepro@gmail.com>
2020-06-07 21:32:22 -0400

    added a row into the database using the django python shell

--- ml_db/mg_links/models.py
+++ ml_db/mg_links/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-# creating my model to add into the database. 
+# creating my model to add into the database.
+
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
     key_word = models.CharField(max_length=200)
     link = models.CharField(max_length=500)
 
-    
+    def __str__(self):
+        return self.link 
