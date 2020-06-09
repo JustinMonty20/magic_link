c7aed9b55197bcd4755fba079a066a201b7bd8b6
Justin Montgomery <montgomerycodepro@gmail.com>
2020-06-09 18:40:53 -0400

    spelling errors

--- ml_db/mg_links/forms.py
+++ ml_db/mg_links/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import Product
 
 class AddProduct(forms.Form):
-    product_name = forms.CharField(max_length=200, label='Product name ')
-    key_word = forms.CharField(max_length=200, label='Key_word ')
-    link = forms.CharField(max_length=500, label='Add link here ') 
+    product_name = forms.CharField(max_length=200, label='Product')
+    key_word = forms.CharField(max_length=200, label='Key_word ', widget=forms.TextInput(attrs={'placeholder': 'Future search term'}))
+    link = forms.CharField(max_length=500, label='Link') 
 
     class Meta:
         model = Product 
 
 
 class SearchProduct(forms.Form):
-    looking_for = forms.CharField(max_length=200, label='Search by key_word ')
+    looking_for = forms.CharField(max_length=200, label='Key_word search')
 
--- ml_db/mg_links/views.py
+++ ml_db/mg_links/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView,View
 from django.template import RequestContext
